29ed8e91faf22a3f760c97a6eec1f64f83d3aaf7
seon54 <seonhyeongpark54@gmail.com>
2019-01-23 23:17:15 +0900

    modify view files
    add mixin
    
    add db for test

---
+++ db.js
@@ -0,0 +1,49 @@
+export const videos = [{
+        id: 1234,
+        title: "Video awesome",
+        description: "This is something I like",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 1423,
+            name: "Tom",
+            email: "tom@abc.com"
+        }
+    },
+    {
+        id: 1224,
+        title: "Video awesome2",
+        description: "This is something I like",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 1423,
+            name: "Tom",
+            email: "tom@abc.com"
+        }
+    },
+    {
+        id: 1243,
+        title: "Video awesome3",
+        description: "This is something I like",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 1423,
+            name: "Tom",
+            email: "tom@abc.com"
+        }
+    },
+    {
+        id: 122334,
+        title: "Video awesome4",
+        description: "This is something I like",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 1423,
+            name: "Tom",
+            email: "tom@abc.com"
+        }
+    }
+];
