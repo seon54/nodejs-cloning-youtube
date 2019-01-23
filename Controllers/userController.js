9ea74b91e99cc4b8d94791e3cc9adbd434781cd8
seon54 <seonhyeongpark54@gmail.com>
2019-01-24 00:32:16 +0900

    modify router

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,28 @@
-export const join = (req, res) => res.render("join", { pageTitle: 'Join' });
-export const login = (req, res) => res.render("login", { pageTitle: 'Log in' });
-export const logout = (req, res) => res.render("logout", { pageTitle: 'Log out' });
+import routes from "../routes"
+
+export const getJoin = (req, res) => {
+    res.render("join", { pageTitle: 'Join' });
+};
+
+export const postJoin = (req, res) => {
+    const {
+        body: { name, email, password, password2 }
+    } = req;
+    if (password !== password2) {
+        res.status(400);
+        res.render("join", { pageTitle: 'Join' });
+    } else {
+        // To Do : 사용자 등록, 사용자 로그인
+        res.redirect(routes.home);
+    }
+};
+export const getLogin = (req, res) => res.render("login", { pageTitle: 'Log in' });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+export const logout = (req, res) => {
+    res.redirect(routes.home);
+}
 export const users = (req, res) => res.serendernd("users", { pageTitle: 'User' });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: 'Edit Profile' });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: 'User Detail' });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,17 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../Controllers/videoController";
-import { join, login, logout } from "../Controllers/userController";
+import { logout, getJoin, postJoin, getLogin, postLogin } from "../Controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin)
+globalRouter.post(routes.login, postLogin);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
